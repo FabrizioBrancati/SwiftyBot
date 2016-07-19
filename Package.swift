df8d30e0bda74ba8af8349bfe49a2fced0a2fbfd
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2016-07-20 00:36:44 +0200

    Minor changes

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "SwiftyBot",
     dependencies: [
         .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 14),
-        .Package(url: "https://github.com/qutheory/vapor-mustache.git", majorVersion: 0, minor: 10)
+        .Package(url: "https://github.com/qutheory/vapor-mustache.git", majorVersion: 0, minor: 10),
         .Package(url: "https://github.com/qutheory/vapor-tls.git", majorVersion: 0, minor: 3)
     ],
     exclude: [
