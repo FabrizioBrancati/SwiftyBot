cf92200b2790414c8fd6c318d85d4719b8d3a744
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2016-08-15 16:27:20 +0200

    Updated repo urls

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@ import PackageDescription
 let package = Package(
     name: "SwiftyBot",
     dependencies: [
-        .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 14),
-        .Package(url: "https://github.com/qutheory/vapor-mustache.git", majorVersion: 0, minor: 10),
-        .Package(url: "https://github.com/qutheory/vapor-tls.git", majorVersion: 0, minor: 3)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 14),
+        .Package(url: "https://github.com/vapor/vapor-mustache.git", majorVersion: 0, minor: 10),
+        .Package(url: "https://github.com/vapor/sqlite-provider.git", majorVersion: 0, minor: 3)
     ],
     exclude: [
         "Config",
