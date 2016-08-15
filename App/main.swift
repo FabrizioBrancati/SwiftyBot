39a514ded745cfb89ee6dc70fd6324e152dc6085
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2016-08-15 17:37:40 +0200

    Updated main.swift

--- App/main.swift
+++ App/main.swift
@@ -4,43 +4,29 @@ let drop = Droplet()
 
 let secret = drop.config["app", "secret"].string ?? ""
 
-drop.get("/") { request in
-    let response = try Response(status: .ok, json: JSON([
-        "status": "OK"
-    ]))
-
-    return response
-}
-
-drop.get(secret) { request in
-    let response = try Response(status: .ok, json: JSON([
-        "status": "OK",
-        "token": "OK"
-    ]))
-
-    return response
-}
-
-drop.get(secret, String.self) { request, method in
-    let response = try Response(status: .ok, json: JSON([
-        "status": "OK",
-        "token": "OK",
-        "method": method
-    ]))
-
-    return response
-}
-
 drop.post(secret) { request in
     let chatID = request.data["message", "chat", "id"].int ?? 0
-    let text = request.data["message", "text"].string ?? ""
-
-    let response = try Response(status: .ok, json: JSON([
-        "chat_id": chatID,
-        "text": text,
-        "method": "sendMessage"
-    ]))
-
+    let message = request.data["message", "text"].string ?? ""
+    
+    var responseMessage: String
+    
+    if message.hasPrefix("/") {
+        switch message {
+        case "/start":
+            responseMessage = "Welcome to SwiftyBot"
+        default:
+            responseMessage = "Command unavailable"
+        }
+    }
+    
+    let response = try Response(status: .ok, json: JSON(
+        [
+            "method": "sendMessage",
+            "chat_id": chatID,
+            "text": responseMessage
+        ])
+    )
+    
     return response
 }
 
