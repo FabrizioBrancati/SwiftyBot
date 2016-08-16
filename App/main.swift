c159ddb633dc8d9fba48743104de111103b356a6
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2016-08-16 19:05:26 +0200

    Update main.swift

--- App/main.swift
+++ App/main.swift
@@ -1,3 +1,4 @@
+
 import Vapor
 
 let drop = Droplet()
@@ -8,22 +9,32 @@ drop.post(secret) { request in
     let chatID = request.data["message", "chat", "id"].int ?? 0
     let message = request.data["message", "text"].string ?? ""
     
-    var responseMessage: String = ""
+    var response: String = ""
     
     if message.hasPrefix("/") {
         switch message {
         case "/start":
-            responseMessage = "Welcome to SwiftyBot"
+            response = "Welcome to SwiftyBot!\n" +
+                       "To list all available commands type /help"
+        case "/help":
+            response = "Welcome to SwiftyBot, an example on how create a Telegram Bot in Swift with Vapor.\n" +
+                       "https://www.fabriziobrancati.com/blog\n\n" +
+                       "/start - Welcome message\n" +
+                       "/help - Help message\n" +
+                       "Any text - Reversed"
         default:
-            responseMessage = "Command unavailable"
+            response = "Unrecognized command.\n" + 
+                       "To list all available commands type /help"
         }
+    } else {
+        response = String(message.characters.reversed())
     }
     
     return try JSON(
         [
             "method": "sendMessage",
             "chat_id": chatID,
-            "text": responseMessage
+            "text": response
         ]
     )
 }
