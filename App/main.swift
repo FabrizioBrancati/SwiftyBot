97068a2c165391614d7fb3580b9b1a72a0abaf33
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2016-08-22 23:12:22 +0200

    Minor changes

--- App/main.swift
+++ App/main.swift
@@ -1,3 +1,29 @@
+//
+//  main.swift
+//  SwiftyBot
+//
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2016 Fabrizio Brancati. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
 import Vapor
 
 /// Create the Droplet.
@@ -30,7 +56,7 @@ drop.post(secret) { request in
         /// Help command "/help".
         case "/help":
             /// Set the response message text.
-            response = "Welcome to SwiftyBot" +
+            response = "Welcome to SwiftyBot " +
                        "an example on how create a Telegram Bot in Swift with Vapor.\n" +
                        "https://www.fabriziobrancati.com/blog\n\n" +
                        "/start - Welcome message\n" +
