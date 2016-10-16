10b118894003f03fd20a579853d30a1af706fdcc
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2016-10-16 19:16:28 +0200

    Updated Vapor 1.1

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftyBot",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 0)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1)
     ],
     exclude: [
         "Config",
