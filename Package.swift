4759892e08094ed5cf5ed6cbe790a06cf333ded2
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2016-12-22 23:46:05 +0100

    Updating Vapor version to 1.3

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftyBot",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 2),
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3),
         .Package(url: "https://github.com/FabrizioBrancati/BFKit-Swift.git", majorVersion: 2, minor: 0)
     ],
     exclude: [
@@ -12,6 +12,6 @@ let package = Package(
         "Localization",
         "Public",
         "Resources",
-        "Tests",
+        "Tests"
     ]
 )
