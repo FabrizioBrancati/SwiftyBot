3abfa96e19901a0510231d9d21d85864d017e68f
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2016-12-23 14:31:18 +0100

    Updating BFKit-Swift to 2.1.x

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "SwiftyBot",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3),
-        .Package(url: "https://github.com/FabrizioBrancati/BFKit-Swift.git", majorVersion: 2, minor: 0)
+        .Package(url: "https://github.com/FabrizioBrancati/BFKit-Swift.git", majorVersion: 2, minor: 1)
     ],
     exclude: [
         "Config",
