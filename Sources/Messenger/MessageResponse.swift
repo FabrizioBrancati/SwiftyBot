b612f9b2d4ca0f74d38efd16d3ee00383b365318
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2018-06-16 18:05:42 +0200

    Correctly handling structured messages

---
+++ Sources/Messenger/MessageResponse.swift
@@ -0,0 +1,61 @@
+//
+//  MessageResponse.swift
+//  SwiftyBot
+//
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2016 - 2018 Fabrizio Brancati.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+import Foundation
+
+/// User message.
+public enum MessageResponse: Codable {
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .text(let text):
+            try container.encode(text)
+        case .structured(let structured):
+            try container.encode(structured)
+        }
+    }
+    
+    enum MessageResponseError: Error {
+        case missingValue
+    }
+    
+    case text(String)
+    case structured(StructuredMessage)
+    
+    public init(from decoder: Decoder) throws {
+        if let text = try? decoder.singleValueContainer().decode(String.self) {
+            self = .text(text)
+            return
+        }
+        
+        if let structured = try? decoder.singleValueContainer().decode(StructuredMessage.self) {
+            self = .structured(structured)
+            return
+        }
+        
+        throw MessageResponseError.missingValue
+    }
+}
