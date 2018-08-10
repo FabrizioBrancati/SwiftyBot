cfa3b8454caf1baa0d421c49b8859406c12f2c97
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2018-08-10 22:22:09 +0200

    Minor changes

--- Tests/telegram.lua
+++ Tests/telegram.lua
@@ -1,3 +1,3 @@
-wrk.method = "POST"
-wrk.body   = "{\"message\":{\"chat\":{\"id\":0},\"text\":\"\\/start this is a test\",\"from\":{\"first_name\":\"Fabrizio\"}}}"
+wrk.method 									= "POST"
+wrk.body   									= "{\"message\":{\"chat\":{\"id\":0},\"text\":\"\\/start this is a test\",\"from\":{\"first_name\":\"Fabrizio\"}}}"
 wrk.headers["Content-Type"] = "application/json"
